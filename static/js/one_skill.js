46e0bdbf0b25fa034c85faddf3b885f6f5cf2858
Kira <kira090308@yandex.ru>
2024-04-27 01:36:54 +0300

    скрытие ненужных окон с ссылками и тд

--- static/js/one_skill.js
+++ static/js/one_skill.js
@@ -16,4 +16,26 @@ function horizontalScroll(element) {
   }
   horizontalScroll(element);
 
-  
+// let topics = document.querySelectorAll(".topic"); 
+// let infWindow = document.querySelector(".links-information")
+// topics.forEach((topic) => {
+//     topic.onclick = function () {
+//         text = topic.querySelector("span:nth-child(2)").textContent;
+//         json.forEach((inf) => function () {
+//             if (text == inf.title) {
+//                 infWindow.querySelector("h2").textContent = inf.title;
+//                 let infUl = infWindow.querySelector("ul");
+//                 links = inf.links;
+//                 links.forEach((link) => {
+//                     let a = document.createElement("a");
+//                     a.textContent = link;
+//                     a.href = link;
+//                     let li = document.createElement("li");
+//                     li.appendChild(a);
+
+
+//                 })
+//             }
+//         })
+//     }
+// })
